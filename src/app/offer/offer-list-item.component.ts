1469fceac1aa9658377795a8eb111aa3e6adb01d
Richard Balog <balog.richard@thisisdone.com>
2017-11-13 22:00:52 +0100

    Type error fix

--- src/app/offer/offer-list-item.component.ts
+++ src/app/offer/offer-list-item.component.ts
@@ -1,8 +1,9 @@
 import {Component, Input} from '@angular/core';
 import {Offer} from '../../models/offer.model';
 import {FileServerService} from '../rest/file-server.service';
-import {AuthenticationService} from "../rest/authentication.service";
-import {Router} from "@angular/router";
+import {AuthenticationService} from '../rest/authentication.service';
+import {Router} from '@angular/router';
+import {User} from '../../models/user.model';
 
 @Component({
     selector: 'sd-offer-list-item',
@@ -21,6 +22,10 @@ export class OfferListItemComponent {
 
     getSignedInUserId() {
         return this._authService.getSignedInUser().id;
+    }
+
+    isSignedInUserOwn() {
+        return ((this.offer.owner === this.getSignedInUserId()) || (this.offer.owner as User).id === this.getSignedInUserId());
     }
 
     editOffer() {
